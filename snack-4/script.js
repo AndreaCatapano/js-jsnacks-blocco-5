5892a3f594bd551c4baf590f14e782587dec9b1f
AndreaCatapano <a.catapano96@gmail.com>
2025-03-12 14:12:27 +0100

    feat: completed snack-4

--- snack-4/script.js
+++ snack-4/script.js
@@ -7,3 +7,8 @@ const posts = [
 
 // Crea un nuovo array coi nomi di tutti gli autori
 // Risultato: ['Marco', 'Luca', 'Fabrizio', 'Enrico']
+
+
+postsAuthor = posts.map(post => post.author);
+
+console.log(postsAuthor);
